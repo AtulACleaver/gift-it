5365db984e71699ce5597c3205e78a33987d32ad
Atul Anand <atulcleaver@gmail.com>
2022-11-27 16:57:47 +0530

    Added items to the table on form submit

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,9 +2,46 @@ import React, { useState } from "react";
 import "../css/Main.css";
 import "../css/Container.css";
 import Popup from "./Popup.jsx";
+import { nanoid } from "nanoid";
+import data from "../data.json";
+import "../css/Table.css";
 
-function Container(props) {
+function Container() {
   const [buttonPopup, setButtonPopup] = useState(false);
+
+  const [tableData, setTableData] = useState(data);
+  const [addFormData, setAddFormData] = useState({
+    id: "",
+    name: "",
+    url: "",
+    idealPrice: "",
+  });
+
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...addFormData };
+    newFormData[fieldName] = fieldValue;
+
+    setAddFormData(newFormData);
+  };
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newGift = {
+      id: nanoid(),
+      name: addFormData.name,
+      url: addFormData.url,
+      idealPrice: addFormData.idealPrice,
+    };
+
+    const newGifts = [...tableData, newGift];
+    setTableData(newGifts);
+  };
 
   return (
     <div className="app-container">
@@ -25,39 +62,81 @@ function Container(props) {
               <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
                 <div className="popup-content">
                   <h1 className="popup-content-heading">
-                    Add a Gift to your List 
+                    Add a Gift to your List
                   </h1>
-                  <div className="popup-content-input">
-                    <p className="input-label">Gift Name:</p>
-                    <input
-                      className="popup-content-input-field"
-                      type="text"
-                      placeholder="Name"
-                    />
-                    <p className="input-label">Amazon Link:</p>
-                    <input
-                      className="popup-content-input-field"
-                      type="url"
-                      placeholder="URL"
-                    />
-                    <p className="input-label">Ideal Price:</p>
-                    <input
-                      className="popup-content-input-field price"
-                      type="number"
-                      placeholder="Price when you want to be alerted"
-                    />
-                    <button
-                      className="popup-content-submit"
-                      onClick={() => setButtonPopup(false)}
-                    >
-                      Submit
-                    </button>
-                  </div>
+                  <form
+                    onSubmit= {handleAddFormSubmit}
+                  >
+                    <div className="popup-content-input">
+                      <p className="input-label">Gift Name:</p>
+                      <input
+                        className="popup-content-input-field"
+                        type="text"
+                        name="name"
+                        required="required"
+                        placeholder="Name"
+                        onChange={handleAddFormChange}
+                      />
+                      <p className="input-label">Amazon Link:</p>
+                      <input
+                        className="popup-content-input-field"
+                        type="url"
+                        name="url"
+                        required="required"
+                        placeholder="URL"
+                        onChange={handleAddFormChange}
+                      />
+                      <p className="input-label">Ideal Price:</p>
+                      <input
+                        className="popup-content-input-field price"
+                        type="number"
+                        name="idealPrice"
+                        required="required"
+                        placeholder="Price when you want to be alerted"
+                        onChange={handleAddFormChange}
+                      />
+                      <button type="submit" className="popup-content-submit">
+                        Submit
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </Popup>
             </div>
           </div>
           <div className="card-table">
+            <div class="table-container">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th className="table-name" scope="col">
+                      Gift Name
+                    </th>
+                    <th className="table-price" scope="col">
+                      Ideal Price
+                    </th>
+                    <th className="table-link" scope="col">
+                      Amazon Link
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tableData.map((item) => (
+                    <tr>
+                      <td>{item.name}</td>
+                      <td className="table-row-price">{item.idealPrice}</td>
+                      <td className="table-row-link">
+                        <button className="table-link-button">
+                          <a href={item.url} className="table-link-button-link">
+                            Buy Now
+                          </a>
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
